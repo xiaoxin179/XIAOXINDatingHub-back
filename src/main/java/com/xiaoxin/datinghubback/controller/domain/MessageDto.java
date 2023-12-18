c191f9a19c3be673d33bec880caa5616497e50d0
xiaoxin179 <97381599+xiaoxin179@users.noreply.github.com>
2023-12-18 17:13:53 +0800

    无法获取头像的聊天

---
+++ src/main/java/com/xiaoxin/datinghubback/controller/domain/MessageDto.java
@@ -0,0 +1,6 @@
+package com.xiaoxin.datinghubback.controller.domain;/**
+*@author:XIAOXIN
+*@date:2023/12/17
+**/
+public class MessageDto {
+}
---
+++ src/main/java/com/xiaoxin/datinghubback/entity/ImWithUser.java
@@ -0,0 +1,9 @@
+package com.xiaoxin.datinghubback.entity;
+
+/**
+ * @author:XIAOXIN
+ * @date:2023/12/18
+ **/
+public class im_with_user {
+    
+}
